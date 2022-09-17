e945445c2732e3e50675e8872ce4d08cd0487926
Floyd-Midaka <FloydMidaka@outlook.com>
2022-09-16 22:32:07 -0700

    C programming

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#definf MAIN_H
+
+/**
+ * main - header file or protypes
+ * void prototypes (void)
+ * int prototypess (int)
+ */
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n)
+void _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+
+#endif /*MAIN _H*/
+
+
